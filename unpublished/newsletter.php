2fd7efb9c4b0b3a41dc99cf4b80f31c8b16ed46b
Carlie Thomas <carlierobinson88@yahoo.com>
2019-08-01 13:03:17 +0100

    Add php to newsletter file
    Ad POST method to form on index page

---
+++ unpublished/newsletter.php
@@ -0,0 +1,19 @@
+<?php
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $email_from = "Python Pro";
+    $email_subject = "You have a new newsletter sign up";
+    $email_body = "Name: $name.\n".
+                    "Email Address: $email.\n";
+    
+    $to = "newsletter@pythonpro.co.uk";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: thanks");
+
+?>
